964010e7ceff6d76cb79145e28e7e173dc529e51
Victor Capetillo <victor.capetillo@usm.cl>
2022-05-15 22:13:05 -0400

    back done

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -9,31 +9,51 @@ exports.getAllNotes = () => {
 }
 
 exports.getNote = (noteId) => {
-    let note = notes.filter((not) => {
-        return not.id === noteId
-    })
-    return note[0]
+    if (0 < noteId && noteId <= notes.length) {
+        let note = notes.filter((not) => {
+            return not.id == noteId
+        })
+        return note[0]
+    }
+    else{
+        return {
+            message: 'Esta Id no se encuentra en el sistema'
+        }
+    }
 }
 
 exports.postNotes = (noteData) => {
-    const note = {
-        id: notes.length + 1,
-        data: noteData.data
+    if (noteData.data != "") {
+        const note = {
+            id: notes.length + 1,
+            data: noteData.data
+        }
+        notes.push(note)
+        return true
     }
-    notes.push(note)
+    else{
+        return false
+    }
 }
 
 exports.removeNote = (noteId) => {
-    let b = 0
-    for (let i = 0; i < notes.length; i++) {
-        if (notes[i].id == noteId){
-            b = i;
-            notes.splice(i, 1)
+    let num_ver = 0
+    if (0 < noteId && noteId <= notes.length) {
+        for (let i = 0; i < notes.length; i++) {
+            if (notes[i].id == noteId){
+                num_ver = i;
+                notes.splice(i, 1)
+            }
+        }
+        for (let k = num_ver; k < notes.length; k++) {
+            notes[k].id = notes[k].id - 1;        
         }
     }
-    for (let k = b; k < notes.length; k++) {
-        notes[k].id = notes[k].id - 1;        
+    else{
+        num_ver = -1
     }
+    
+    return num_ver
 }
 
 exports.updateNote = (noteId, noteData) => {
@@ -41,7 +61,6 @@ exports.updateNote = (noteId, noteData) => {
     for (let i = 0; i < notes.length; i++) {
         if (notes[i].id == noteId){
             notes[i].data = noteData
-            //console.log(notes[i])
         }
     }
 }
--- src/routes/notes/notes.js
+++ src/routes/notes/notes.js
@@ -7,21 +7,43 @@ exports.getAllNotes = (ctx) => {
 
 exports.getNote = (ctx) => {
     ctx.body = notesActions.getNote(ctx.params.id)
+    console.log(ctx.body)
+    
+    if(ctx.body == null){
+        console.log(ctx.body)
+        ctx.body = { message: 'Esta Id no se encuentra en el sistema' }
+    }
+    console.log(ctx.body)
     return ctx
 }
 
 exports.removeNote = (ctx) => {
-    notesActions.removeNote(ctx.params.id)
-    ctx.body = {
-        message: 'Note was removed successfully'
+    let numero = notesActions.removeNote(ctx.params.id)
+    console.log(ctx.params.id)
+    console.log(numero)
+    if(numero !== -1){
+        ctx.body = {
+            message: 'Note was removed successfully.'
+        }
+    }
+    else{
+        ctx.body = {
+            message: 'Note doesnt exit so it cant be removed.'
+        }
     }
     return ctx
 }
+//notes/notes.js
 
 exports.postNotes = (ctx) => {
-    notesActions.postNotes(ctx.request.body)
-    ctx.body = { message: 'Note created successfully' }
-    return ctx
+    let verif = notesActions.postNotes(ctx.request.body)
+    if (verif == true) {
+        ctx.body = { message: 'Note created successfully' }
+    }
+    else{
+        ctx.body = { message: 'Estas agregando una nota vacia!'}
+    }
+    return ctx   
 }
 
 exports.updateNote = (ctx) => {
